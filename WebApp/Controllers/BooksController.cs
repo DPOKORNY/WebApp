995741f456ffc5fead341cf1434d78aaf722cf0e
Dan Pokorný <danpokorny01@gmail.com>
2017-08-15 11:36:35 +0200

    1.commit

---
+++ WebApp/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            Book book = new Book() {Author="Dan Pokorný",Name="Kniha XY",Id=5,PublishedYear=2014 };
+
+            
+            return View();
+        }
+    }
+}
